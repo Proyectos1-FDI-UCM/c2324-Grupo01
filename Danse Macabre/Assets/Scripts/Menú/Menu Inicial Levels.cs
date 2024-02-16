a0a857ecf8db95f70380b2fd770bf1b76e9a9aa4
Odisky712 <zhuzhu@ucm.es>
2024-02-16 16:44:38 +0100

    UI de Menu Inicial y Level + Paquete de texto

---
+++ Danse Macabre/Assets/Scripts/Menú/Menu Inicial Levels.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuInicialLevels : MonoBehaviour
+{
+    public void Jugar_Tutorial() 
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Jugar_Nivel1() 
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void Jugar_Nivel2() 
+    {
+        SceneManager.LoadScene(3);
+    }
+
+}
