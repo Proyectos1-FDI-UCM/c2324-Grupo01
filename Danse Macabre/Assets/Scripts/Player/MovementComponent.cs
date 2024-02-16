22892c8044b1726657daa98b0f2ae8bbd5a26b61
GabrielBarrosoBuenoUCM <160004431+GabrielBarrosoBuenoUCM@users.noreply.github.com>
2024-02-16 11:59:03 +0100

    Cambios a la gravedad
    a

--- Danse Macabre/Assets/Scripts/Player/MovementComponent.cs
+++ Danse Macabre/Assets/Scripts/Player/MovementComponent.cs
@@ -7,22 +7,45 @@ public class MovementComponent : MonoBehaviour
 {
     [SerializeField]
     float speed;
+    [SerializeField]
+    float maxSpeed = -100f;
 
     bool canMove = true;
+    
+    float verticalSpeed = 0;
 
     Transform myTransform;
+    Rigidbody2D myRigidBody;
 
     // Start is called before the first frame update
     void Start()
     {
         myTransform = transform;
+        myRigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (canMove = true) {
-            myTransform.position = new Vector3(myTransform.position.x + (speed * Time.deltaTime), myTransform.position.y, myTransform.position.z);
+        Gravity();
+        Move();
+    }
+    private void Move()
+    {
+        if (canMove)
+        {
+            Vector3 distanceToMove = new Vector3 (speed, verticalSpeed, 0) * Time.deltaTime;
+
+            myRigidBody.MovePosition(myTransform.position + distanceToMove);
         }
+    }
+    private void Gravity()
+    {
+        Debug.Log(verticalSpeed + Physics.gravity.y * 0.01f);
+        verticalSpeed = Mathf.Max(maxSpeed, verticalSpeed + Physics.gravity.y * 0.01f);
+
+        /*Other iterations:
+        verticalSpeed = Mathf.Max(minSpeed, verticalSpeed + Physics.gravity.y * Time.deltaTime);
+        verticalSpeed += Physics.gravity.y * 0.01f *Time.deltaTime;*/
     }
 }
