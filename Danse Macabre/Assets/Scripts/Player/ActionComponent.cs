3073f0354e4db5d3e66cf22c30e3ab11007031b0
Hugodbr <hdebrito@ucm.es>
2024-02-17 17:44:15 +0100

    Stomp

--- Danse Macabre/Assets/Scripts/Player/ActionComponent.cs
+++ Danse Macabre/Assets/Scripts/Player/ActionComponent.cs
@@ -6,8 +6,13 @@ public class ActionComponent : MonoBehaviour
 {
     #region parameters
     [SerializeField]
-    private float _jumpSpeed = 1.0f;
-    private float groundCheckDistance = 0.1f;
+    private float _jumpSpeed = 9.0f;
+    [SerializeField]
+    private float groundCheckDistance = 0.55f; // Estaba pequeña
+    [SerializeField]
+    private float impulseStomp = 20;
+    [SerializeField]
+    private float impulseTrampolin = 12;
     #endregion
 
     #region references
@@ -18,9 +23,11 @@ public class ActionComponent : MonoBehaviour
     #endregion
 
     #region properties
+    private bool _stomping = false; // sirve para habilitar rebote del trampolin
     private float _verticalSpeed;
-    #endregion 
+    #endregion
 
+    #region methods
     //CAMBIAD LO QUE HACE CADA METODO!! (LO QUE HAY ES PARA PROBAR!)
     private bool IsGrounded()
     {
@@ -38,14 +45,35 @@ public class ActionComponent : MonoBehaviour
             _myRB.velocity = new Vector2(_myRB.velocity.x, _jumpSpeed);
         }
     }
+
+    // DONE
     public void Stomp()
     {
-        gameObject.SetActive(true);
+        if (!IsGrounded() && !_stomping)
+        {
+            _stomping = true;
+            _myRB.AddForce(impulseStomp * Vector2.down, ForceMode2D.Impulse);
+        }
     }
+
     public void Slide()
     {
         _myTransform.localScale = new Vector3(0.5f, 0.5f, 1);
     }
+
+    // DONE
+    // Rebote del trampolín con collider isTrigger
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (_stomping && collision.gameObject.tag == "Trampolin")
+        {
+            _myRB.velocity = Vector2.zero;
+            _myRB.AddForce(impulseTrampolin * Vector2.up, ForceMode2D.Impulse);
+            _stomping = false;
+        }
+    }
+    #endregion
+
     // Start is called before the first frame update
     void Start()
     {
@@ -56,6 +84,6 @@ public class ActionComponent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
