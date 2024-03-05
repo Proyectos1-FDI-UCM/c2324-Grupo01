eb8fe596c56280e85fe8496bcc01bc18e1ff3dc6
Zhuokai Zhu <zhuzhu@ucm.es>
2024-03-05 18:22:56 +0100

    Fix al cambiar de nivel, se arranca el juego

--- Danse Macabre/Assets/Scripts/Menú/Menu Inicial Levels.cs
+++ Danse Macabre/Assets/Scripts/Menú/Menu Inicial Levels.cs
@@ -7,14 +7,17 @@ public class MenuInicialLevels : MonoBehaviour
 {
     public void Jugar_Tutorial() 
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(1);
     }
     public void Jugar_Nivel1() 
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(2);
     }
     public void Jugar_Nivel2() 
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(3);
     }
 }
