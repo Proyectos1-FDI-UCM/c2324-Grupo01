4b794cf5aff336591b8841002ec7ef620854f11b
Zhuokai Zhu <zhuzhu@ucm.es>
2024-03-07 16:21:51 +0100

    Cambios script de muerte de estructura para menuMuerte (puntuación)

--- Danse Macabre/Assets/Scripts/Scene objects/DeathComponent.cs
+++ Danse Macabre/Assets/Scripts/Scene objects/DeathComponent.cs
@@ -1,24 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DeathComponent : MonoBehaviour
 {
     #region references
     private GameManager _gameManager;
+    private ScoreManager _scoreManager;
     #endregion
     private void OnCollisionEnter2D(Collision2D collision)
     {
         MovementComponent _player = collision.gameObject.GetComponent<MovementComponent>();
         if (_player != null)
         {
-            _gameManager.Muerte(); //muere personaje
+            _scoreManager.SaveFinalScore();
+            //Cambiar escena de muerte
+            SceneManager.LoadScene(6);
         }
     }
 
     void Start()
     {
         _gameManager = FindObjectOfType<GameManager>();
+        _scoreManager = FindObjectOfType<ScoreManager>();
     }
 }
 
