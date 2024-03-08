d19f0f325f47108b953ec1f0ffffbdc60ea07cf7
hdebrito92 <hdebrito@ucm.es>
2024-03-08 03:03:48 +0100

    build Danse Macabre.exe

--- Danse Macabre/Assets/Scripts/Player/ActionComponent.cs
+++ Danse Macabre/Assets/Scripts/Player/ActionComponent.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.Design;
-using TMPro.EditorUtilities;
 using Unity.Burst.CompilerServices;
 using UnityEngine;
 
