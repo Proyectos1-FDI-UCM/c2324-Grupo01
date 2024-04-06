f2fb690098ecbd0a1aae7921fd97815d91c68676
Haoshuang <haohou@ucm.es>
2024-04-06 02:47:08 +0200

    TextDiferenteCOLOR

--- Danse Macabre/Assets/Scripts/UI/TimingTextController.cs
+++ Danse Macabre/Assets/Scripts/UI/TimingTextController.cs
@@ -7,6 +7,7 @@ public class TimingTextController : MonoBehaviour
 {
     #region references
     [SerializeField] TextMeshPro _timingText;
+    [SerializeField] Color[] colors = new Color[5];
     #endregion
 
     #region parameters
@@ -16,7 +17,27 @@ public class TimingTextController : MonoBehaviour
     #region methods
     public void TimingText(string timing)
     {
-       _timingText.text = timing;
+        if (timing == "PERFECT")
+        {
+            _timingText.color = colors[0];
+        }
+        else if (timing == "GREAT")
+        {
+            _timingText.color = colors[1];
+        }
+        else if (timing == "GOOD")
+        {
+            _timingText.color = colors[2];
+        }
+        else if (timing == "WRONG")
+        {
+            _timingText.color = colors[3];
+        }
+        else if (timing == "MISSED")
+        {
+            _timingText.color = colors[4];
+        }
+        _timingText.text = timing;
         Invoke("resetText", _resetTime);
     }
     private void resetText()
