5464a809a0b1282ddb4e0da73f53e61aeeb1dbb2
hdebrito92 <hdebrito@ucm.es>
2024-04-24 10:51:39 +0200

    victory scene next restart in progress

---
+++ Danse Macabre/Assets/Scripts/GameManager/SceneLoadManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoadManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // private void OnSceneLoaded(Scene scene, LoadSceneMode mode) // Runs whenever a scene is loaded
+    // { 
+    //     GameManager.Instance.DebugGM();
+    // }
+    // void OnEnable()
+    // {
+    //     SceneManager.sceneLoaded += OnSceneLoaded;
+    // }
+    // void OnDisable()
+    // {
+    //     SceneManager.sceneLoaded -= OnSceneLoaded;
+    // }
+}
