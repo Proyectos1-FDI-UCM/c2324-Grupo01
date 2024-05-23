f5b6171b75c2ebdd54ba0073d5eb368826ddbdd5
Haoshuang <haohou@ucm.es>
2024-05-23 19:00:23 +0200

    organizacion

---
+++ Danse Macabre/Assets/Scripts/UI/TutorialTextController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class TutorialTextController : MonoBehaviour
+{
+    public void DisplayTiming(string timing)
+    {
+        // if (timing == "PERFECT") {print("Perfect!!!");}
+        // else if (timing == "GOOD") {print("Good!");}
+        // else if (timing == "BAD") {print("Bad...");}
+        // else if (timing == "WRONG") {print("WRONG MOVE!!!");}
+        // else if (timing == "MISSED") {print("MISSED!");}
+    }
+
+}
